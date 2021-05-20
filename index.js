c341e84d1e57866441efcd2934c421bf3ab886f3
Артем Белышев <artembelysev@MacBook-Pro-Artem.local>
2021-05-20 21:55:08 +0200

    added burger menu

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ window.addEventListener('scroll', () => {
     imageCode.style.transform = `translateY(-${diff / 4}px)`
   }
 });
+
+var close = document.getElementById('close');
+var close1 = document.getElementById('close1');
+var close2 = document.getElementById('close2');
+var close3 = document.getElementById('close3');
+var hidd = document.getElementById('hiddenMenu1');
+
+close.addEventListener('click', () => {
+  close1.classList.toggle('rotate_close1');
+  close2.classList.toggle('transform_close2');
+  close3.classList.toggle('rotate_close3');
+  hidd.classList.toggle('showHidden');
+});
